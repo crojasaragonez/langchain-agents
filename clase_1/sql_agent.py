5d7a84a2eabd3e4cf0e5df430bd4f3eec3bd508f
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2026-01-21 19:13:22 -0600

    sql_agent completed

--- clase_1/sql_agent.py
+++ clase_1/sql_agent.py
@@ -2,6 +2,7 @@ from langchain.chat_models import init_chat_model
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain.agents import create_agent
+from langchain_core.chat_history import InMemoryChatMessageHistory
 
 class SQLAgent:
 
@@ -15,6 +16,7 @@ class SQLAgent:
     self.toolkit = None
     self.tools = None
     self.agent = None
+    self.memory = InMemoryChatMessageHistory()
     self._setup()
 
   def _setup(self):
@@ -49,8 +51,35 @@ can query. Do NOT skip this step.
 Then you should query the schema of the most relevant tables.
 """
   def query(self, question):
-    for step in self.agent.stream(
-      {"messages": [{"role": "user", "content": question}]},
-      stream_mode="values"
-    ):
+    self.memory.add_user_message(question)
+    input_messages = self._prepare_messages()
+    last_messages = self._stream_agent_response(input_messages)
+    self._save_assistant_response(last_messages)
+
+  def _save_assistant_response(self, messages):
+    for msg in reversed(messages):
+      if msg.type == 'ai':
+        content = msg.content if isinstance(msg.content, str) else str(msg.content)
+        self.memory.add_ai_message(content)
+
+  def _stream_agent_response(self, input_messages):
+    last_messages = None
+    for step in self.agent.stream({"messages": input_messages}, stream_mode="values"):
       step["messages"][-1].pretty_print()
+      last_messages = step["messages"]
+    return last_messages
+
+  def _prepare_messages(self):
+    role_map = {
+      "human": "user",
+      "ai": "assistant",
+      "system": "system",
+      "tool": "tool"
+    }
+    # { role: user, content: "cuantas facturas hay?" }
+    # { role: ai, content: "hay 150 facturas"}
+    return [
+      { "role": role_map.get(m.type, "user"), "content": m.content or "" }
+      for m in self.memory.messages
+    ]
+
